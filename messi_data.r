ca7915cdca80307476966b435006053559c5950d
Darren Miller <darren.miller1992@hotmail.co.uk>
2020-07-06 21:45:35 +0100

    Significant change to data compiled, due to seasons having to be filtered at the first stage, and cannot be done at a later stage.

--- messi_data.r
+++ messi_data.r
@@ -7,25 +7,97 @@ library(StatsBombR)
 library(FC.rSTATS)
 library(SBpitch)
 
-la_liga <- FreeCompetitions() %>% 
-  filter(competition_id == 11)
+## la liga datasets
 
-champ_lg <- FreeCompetitions() %>% 
-  filter(competition_id == 16)
+la_liga_06_07 <- FreeCompetitions() %>% 
+  filter(competition_id == 11 & season_name == '2006/2007')
 
-matches_esp <- FreeMatches(la_liga)
+la_liga_07_08 <- FreeCompetitions() %>% 
+  filter(competition_id == 11 & season_name == '2007/2008')
 
-matches_cl <- FreeMatches(champ_lg)
+la_liga_08_09 <- FreeCompetitions() %>% 
+  filter(competition_id == 11 & season_name == '2008/2009')
 
-## convert to dataframes
+la_liga_09_10 <- FreeCompetitions() %>% 
+  filter(competition_id == 11 & season_name == '2009/2010')
 
-matches_esp <- StatsBombFreeEvents(MatchesDF = matches_esp, Parallel = T)
+la_liga_10_11 <- FreeCompetitions() %>% 
+  filter(competition_id == 11 & season_name == '2010/2011')
 
-matches_cl <- StatsBombFreeEvents(MatchesDF = matches_cl, Parallel = T)
+la_liga_11_12 <- FreeCompetitions() %>% 
+  filter(competition_id == 11 & season_name == '2011/2012')
 
-## clean the data 
+la_liga_12_13 <- FreeCompetitions() %>% 
+  filter(competition_id == 11 & season_name == '2012/2013')
 
-matches_esp <- allclean(matches_esp)
+la_liga_13_14 <- FreeCompetitions() %>% 
+  filter(competition_id == 11 & season_name == '2013/2014')
 
-matches_cl <- allclean(matches_cl)
+la_liga_14_15 <- FreeCompetitions() %>% 
+  filter(competition_id == 11 & season_name == '2014/2015')
 
+la_liga_15_16 <- FreeCompetitions() %>% 
+  filter(competition_id == 11 & season_name == '2015/2016')
+
+la_liga_16_17 <- FreeCompetitions() %>% 
+  filter(competition_id == 11 & season_name == '2016/2017')
+
+la_liga_17_18 <- FreeCompetitions() %>% 
+  filter(competition_id == 11 & season_name == '2017/2018')
+
+la_liga_18_19 <- FreeCompetitions() %>% 
+  filter(competition_id == 11 & season_name == '2018/2019')
+
+# data preparation 
+
+la_liga_06_07 <- FreeMatches(la_liga_06_07)
+la_liga_06_07 <- StatsBombFreeEvents(MatchesDF = la_liga_06_07, Parallel = T)
+la_liga_06_07 <- allclean(la_liga_06_07)
+
+la_liga_07_08 <- FreeMatches(la_liga_07_08)
+la_liga_07_08 <- StatsBombFreeEvents(MatchesDF = la_liga_07_08, Parallel = T)
+la_liga_07_08 <- allclean(la_liga_07_08)
+
+la_liga_08_09 <- FreeMatches(la_liga_08_09)
+la_liga_08_09 <- StatsBombFreeEvents(MatchesDF = la_liga_08_09, Parallel = T)
+la_liga_08_09 <- allclean(la_liga_08_09)
+
+la_liga_09_10 <- FreeMatches(la_liga_09_10)
+la_liga_09_10 <- StatsBombFreeEvents(MatchesDF = la_liga_09_10, Parallel = T)
+la_liga_09_10 <- allclean(la_liga_09_10)
+
+la_liga_10_11 <- FreeMatches(la_liga_10_11)
+la_liga_10_11 <- StatsBombFreeEvents(MatchesDF = la_liga_10_11, Parallel = T)
+la_liga_10_11 <- allclean(la_liga_10_11)
+
+la_liga_11_12 <- FreeMatches(la_liga_11_12)
+la_liga_11_12 <- StatsBombFreeEvents(MatchesDF = la_liga_11_12, Parallel = T)
+la_liga_11_12 <- allclean(la_liga_11_12)
+
+la_liga_12_13 <- FreeMatches(la_liga_12_13)
+la_liga_12_13 <- StatsBombFreeEvents(MatchesDF = la_liga_12_13, Parallel = T)
+la_liga_12_13 <- allclean(la_liga_12_13)
+
+la_liga_13_14 <- FreeMatches(la_liga_13_14)
+la_liga_13_14 <- StatsBombFreeEvents(MatchesDF = la_liga_13_14, Parallel = T)
+la_liga_13_14 <- allclean(la_liga_13_14)
+
+la_liga_14_15 <- FreeMatches(la_liga_14_15)
+la_liga_14_15 <- StatsBombFreeEvents(MatchesDF = la_liga_14_15, Parallel = T)
+la_liga_14_15 <- allclean(la_liga_14_15)
+
+la_liga_15_16 <- FreeMatches(la_liga_15_16)
+la_liga_15_16 <- StatsBombFreeEvents(MatchesDF = la_liga_15_16, Parallel = T)
+la_liga_15_16 <- allclean(la_liga_15_16)
+
+la_liga_16_17 <- FreeMatches(la_liga_16_17)
+la_liga_16_17 <- StatsBombFreeEvents(MatchesDF = la_liga_16_17, Parallel = T)
+la_liga_16_17 <- allclean(la_liga_16_17)
+
+la_liga_17_18 <- FreeMatches(la_liga_17_18)
+la_liga_17_18 <- StatsBombFreeEvents(MatchesDF = la_liga_17_18, Parallel = T)
+la_liga_17_18 <- allclean(la_liga_17_18)
+
+la_liga_18_19 <- FreeMatches(la_liga_18_19)
+la_liga_18_19 <- StatsBombFreeEvents(MatchesDF = la_liga_18_19, Parallel = T)
+la_liga_18_19 <- allclean(la_liga_18_19)
